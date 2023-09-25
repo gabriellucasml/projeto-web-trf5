2574fa2f7728853f4f5cf752fd8dbaa23e6a104a
Leonardo Lucena <leonardolucena.cc@gmail.com>
2023-09-25 14:25:58 -0300

    :bug: fix: Imports

--- api-triagem/src/main/java/com/triagem/api/Model/Credenciais.java
+++ api-triagem/src/main/java/com/triagem/api/Model/Credenciais.java
@@ -1,6 +1,5 @@
 package com.triagem.api.Model;
 
-import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SQLDelete;
@@ -10,6 +9,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
